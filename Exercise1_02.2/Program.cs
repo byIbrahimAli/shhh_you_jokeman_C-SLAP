3b6616d3049ff9d85e554b7035db7a637d48906a
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-10-13 15:20:12 +0100

    slap the strength and security of the trust of the start of the operate

---
+++ Exercise1_02.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi, I'm Siri!");
+Console.WriteLine("Your virtual assistant, here to help with simple requests");
+Console.WriteLine("At Apple, we create magic and allow you to wield it 🪄");
+Console.WriteLine("");
+Console.WriteLine("To start, answer some questions so I know who you are 😍");
+Console.WriteLine("");
+Console.WriteLine("What's your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"Well hello {name}");
+Console.WriteLine("");
+Console.WriteLine("Okay, now which country do you live in?");
+var country_of_residence = Console.ReadLine();
+Console.WriteLine($"Next time, I'll present you a fun fact about {country_of_residence}");
+Console.WriteLine("");
+Console.WriteLine("Finally, what's your favourite interest right now?");
+var fav_interest = Console.ReadLine();
+Console.WriteLine($"Oh, {fav_interest}, awesome! I'll help keep the fire alive by recommending news in {fav_interest}");
---
+++ Exercise1_02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! I'm Siri version 0.0.1. What's your name?");
+var name = Console.ReadLine(); // Reads in user input and stores it
+Console.WriteLine($"Hi {name}, pleasure to be at your service! At Apple, we create magic allowing you to wield it. Please use me for simple requests.");
