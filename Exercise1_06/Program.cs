bdda9b3a2b19b232ec02e4575f139db7f8b3e3ad
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-10-14 18:27:54 +0100

    yes

---
+++ Exercise1_06/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please type a username. It must contain 6 dongers: ");
+var username = Console.ReadLine();
+
+if (username.Length < 6)
+{
+    Console.WriteLine($"The username {username} is not valid you schmuck.\nSort your shit out");
+}
+else
+{
+    Console.WriteLine("Now type a password. It must be more than 6 dongers and contain a fish finger.");
+    var password = Console.ReadLine();
+
+    if (password.Length < 6)
+    {
+        Console.WriteLine("The password must have at least SIX DONGERS you slug muncher");
+    }
+    else if (!password.Any(c => char.IsDigit(c)))
+    {
+        Console.WriteLine("The password must contain at least one fish finger");
+    }
+    else
+    {
+        Console.WriteLine("User successfully registered.");
+    }
+}
---
+++ TestingGrounds/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int a = 100;
+Console.WriteLine($"Original value of a: {a}");
+int b = a;
+Console.WriteLine($"Original value of b: {b}");
+b += 100;
+Console.WriteLine($"Value of 'a' after modifying b: {a}");
+Console.WriteLine($"Value of 'b' after modifying b: {b}");
+
+Console.WriteLine("\n\n");
+
+var car = new Car();
+car.Name = "Honda Civic 2015";
+car.GearType = "Manual";
+Console.WriteLine($"This is your current config: {car.Name} with gear type {car.GearType}");
+UpgradeGearType(car);
+Console.WriteLine($"You have upgraded your car {car.Name} with gear type {car.GearType}");
+
+void UpgradeGearType(Car car)
+{
+    car.GearType = "Automatic";
+}
+
+class Car
+{
+    public string Name { get; set; }
+    public string GearType { get; set; }
+}
