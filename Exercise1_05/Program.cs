fc7265909bf7edb055c195e5a12ff27ab4f79460
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-10-14 15:03:03 +0100

    they say snails were once human boogers

---
+++ Exercise1_05/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Are the local and utc dates equal? {0}", DateTime.Now.Date == DateTime.UtcNow.Date);
+
+Console.WriteLine("\nIf the dates are equal, does it mean that there's no TimeSpan interval between them? {0}", (DateTime.Now.Date - DateTime.UtcNow.Date) == TimeSpan.Zero);
+
+DateTime localTime = DateTime.Now;
+DateTime utcTime = DateTime.UtcNow;
+TimeSpan interval = (localTime - utcTime);
+
+Console.WriteLine("\nDifference between the {0} Time and {1} Time: {2}:{3} hours", localTime.Kind.ToString(), utcTime.Kind.ToString(), interval.Hours, interval.Minutes);
+
+Console.Write("\nIf we jump two days to the future on {0} we'll be on {1}", new DateTime(2020, 12, 31).ToShortDateString(), new DateTime(2020, 12, 31).AddDays(2).ToShortDateString());
+
+var frenchDate = new DateTime(2008, 3, 1, 7, 0, 0);
+Console.WriteLine("\n\n{0}", frenchDate.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("fr-FR")));
+
+var usDate = new DateTime(2008, 3, 1, 7, 0, 0);
+Console.WriteLine("\n{0}", usDate.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US")));
